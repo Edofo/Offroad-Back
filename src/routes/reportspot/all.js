15696768ee6cabc75d03ec605477d1669da90f9f
Edofo <Edofo@users.noreply.github.com>
2021-03-25 14:33:16 +0100

    Add report spot

--- src/routes/reportspot/all.js
+++ src/routes/reportspot/all.js
@@ -4,17 +4,18 @@ import { PrismaClient } from '@prisma/client'
 const api = Router()
 
 api.get('/:id', async (req, res) => {
-    const id = parseInt(req.params.id)
+    const spotId = parseInt(req.params.id)
 
     const prisma = new PrismaClient()
+
     try {
-        const user = await prisma.user.findFirst({
+        const report = await prisma.report.findFirst({
             where: {
-                id
+                spotId
             }
         })
 
-        res.json({ data: { user } })
+        res.json({ data: { report } })
     } catch (err) {
         res.status(400).json({ error: err.message })
     }
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import auth from './auth/router'
 import spot from './spot/router'
+import reportspot from './reportspot/router'
 import secured from './secured/router'
 import passport from 'passport'
 
@@ -12,6 +13,7 @@ router.get('/', (_req, res) => {
 
 router.use('/auth', auth)
 router.use('/spot', spot)
+router.use('/reportspot', reportspot)
 router.use('/users', passport.authenticate('jwt', { session: false }), secured)
 
 export default router
