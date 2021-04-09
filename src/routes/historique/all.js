8c8b80c7e4188388953718e7496611d4009fa263
Edofo <Edofo@users.noreply.github.com>
2021-04-09 14:39:25 +0200

    Add historique

---
+++ src/routes/historique/all.js
@@ -0,0 +1,23 @@
+import { Router } from 'express'
+
+import prisma from '../../db'
+
+const api = Router()
+
+api.get('/:id', async (req, res) => {
+    try {
+        const authorId = parseInt(req.params.id)
+
+        const historique = await prisma.historique.findMany({
+            where: {
+                authorId
+            }
+        })
+
+        res.json({ data: { historique } })
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+})
+
+export default api
---
+++ src/routes/historique/create.js
@@ -0,0 +1,55 @@
+import { Router } from 'express'
+import { isEmpty } from 'lodash'
+import { convertSpeed } from 'geolib'
+
+import prisma from '../../db'
+
+const api = Router()
+
+api.post('/', async (req, res) => {
+    try {
+
+        const acceptedFields = ['distance', 'temps', 'speed', 'spotId', 'authorId']
+
+        const missingValues = acceptedFields.filter(field => !req.body[field])
+        if (!isEmpty(missingValues)) {
+            return res.status(400).json({
+            error: `Values ${missingValues.join(', ')} are missing`
+            })
+        }
+
+        const { distance, temps, speed, spotId, authorId } = req.body
+
+
+        function dateHMS(temps) {
+            const addZero = function(v) { return v<10 ? '0' + v : v; };
+            const d = new Date(temps * 1000);
+            const t = [];
+            t.push(addZero(d.getHours())+ 'h');
+            t.push(addZero(d.getMinutes())+ 'm');
+            return t.join(' ');
+        }
+
+        const vMoyen = await Math.floor((distance / temps) * 3.6) + ' km';
+        const time = await dateHMS(temps)
+        const longueur = await distance + ' km/h';
+        const max = await Math.floor(convertSpeed(speed, 'kmh')) + ' km/h';
+    
+        const historique = await prisma.historique.create({
+            data: {
+                vMoyen,
+                time,
+                longueur,
+                max,
+                spotId: spotId,
+                authorId: authorId
+            }
+        })  
+
+        res.json({ data: { historique } })
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+})
+
+export default api
