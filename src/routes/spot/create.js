6428abfbceee0727fa3c2783101e31705c72b96e
Edofo <Edofo@users.noreply.github.com>
2021-04-09 19:39:28 +0200

    Update create.js

--- src/routes/spot/create.js
+++ src/routes/spot/create.js
@@ -18,6 +18,18 @@ api.post('/', async (req, res) => {
 
         const { level, adress, infos } = req.body
 
+        if(adress != ""){ // Si l'adresse n'est pas vide
+            let geocoder =  new google.maps.Geocoder(); // On instancie le geocoder
+            geocoder.geocode( { 'address': adress}, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) { // Si l'adresse a été résolue
+                    lat = results[0].geometry.location.lat(); // On récupère la latitude
+                    lon = results[0].geometry.location.lng(); // On récupère la longitude
+                    console.log(lat)
+                } else {
+                    alert("Something got wrong " + status);
+                }
+            });
+        }
 
         const spot = await prisma.spot.create({
             data: {
