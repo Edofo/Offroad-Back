f81920c995b6fb354e03fa757e0c10c173de0793
Seb-Pere <70896756+Seb-Pere@users.noreply.github.com>
2021-04-14 23:24:13 +0200

    test

--- src/routes/spot/create.js
+++ src/routes/spot/create.js
@@ -19,19 +19,19 @@ api.post('/', async (req, res) => {
 
         const { level, adress, infos, noteUser, content, authorId, userName } = req.body
 
-        /*axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+        axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
             params:{
                 address: adress,
                 key: process.env.GOOGLE_API_KEY
             }
-        })*/
+        })
         
-        axios.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json',{
+        /*axios.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json',{
             params:{
                 input: adress,
                 key: process.env.GOOGLE_API_KEY
             }
-        })
+        })*/
         .then(async function(response){
 
             const spot = await prisma.spot.create({
@@ -39,10 +39,10 @@ api.post('/', async (req, res) => {
                     level,
                     adress,
                     infos,
-                    /*lat: response.data.results[0].geometry.location.lat,
-                    lng: response.data.results[0].geometry.location.lng,*/
-                    lat: response.data.candidates[0].geometry.location.lat,
-                    lat: response.data.candidates[0].geometry.location.lng,
+                    lat: response.data.results[0].geometry.location.lat,
+                    lng: response.data.results[0].geometry.location.lng,
+                    /*lat: response.data.candidates[0].geometry.location.lat,
+                    lat: response.data.candidates[0].geometry.location.lng,*/
                     note: noteUser,
                 }
             })
