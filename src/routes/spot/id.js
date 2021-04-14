c1e994def3bca66870dad8ca838b78f83580fd37
Edofo <Edofo@users.noreply.github.com>
2021-04-14 10:12:10 +0200

    Add spot unique

---
+++ src/routes/spot/id.js
@@ -0,0 +1,33 @@
+import { Router } from 'express'
+import prisma from '../../db'
+import { isEmpty } from 'lodash'
+
+const api = Router()
+
+api.get('/:id', async (req, res) => {
+  try {
+
+    const acceptedFields = ['id']
+
+    const missingValues = acceptedFields.filter(field => !req.params.id)
+    if (!isEmpty(missingValues)) {
+      return res.status(400).json({
+        error: `Values ${missingValues.join(', ')} are missing`
+      })
+    }
+
+    const level  = req.params.level
+
+    const spot = await prisma.spot.findMany({
+      where: {
+        id
+      }
+    })
+
+    res.json({ data: { spot } })
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+})
+
+export default api
--- src/routes/spot/router.js
+++ src/routes/spot/router.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import all from './all'
+import id from './id'
 import create from './create'
 import del from './delete'
 import update from './edit'
@@ -7,6 +8,7 @@ import update from './edit'
 const api = Router()
 
 api.use('/', all)
+api.use('/unique/', id)
 api.use('/add', create)
 api.use('/delete', del)
 api.use('/edit', update)
