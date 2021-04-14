ccfaffc7cc09a4ef601be5698da17ae34d8a2219
Edofo <Edofo@users.noreply.github.com>
2021-04-14 15:15:08 +0200

    Update allspot.js

--- src/routes/post/allspot.js
+++ src/routes/post/allspot.js
@@ -7,26 +7,14 @@ api.get('/:id', async (req, res) => {
     try {
         const id = parseInt(req.params.id)
 
+
         const post = await prisma.post.findMany({
             where: {
                 spotId: id
             }
-        }).then(async(response) => {
-
-            const user = await prisma.user.findMany({
-                where: {
-                    id: response.id
-                },
-                select: {
-                    pseudo: true,
-                    level: true
-                }
-            })
-
-            res.json({ data: { post } })
         })
 
-        
+        res.json({ data: { post } })
     } catch (err) {
         res.status(400).json({ error: err.message })
     }
