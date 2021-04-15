8963ae4bd72ed7c86d8d22d139696c738ca1f8e0
Edofo <Edofo@users.noreply.github.com>
2021-04-15 12:53:15 +0200

    Update create.js

--- src/routes/post/create.js
+++ src/routes/post/create.js
@@ -7,7 +7,7 @@ const api = Router()
 api.post('/', async (req, res) => {
     try {
 
-        const acceptedFields = ['content', 'noteUser', 'postId', 'authorId', 'userName']
+        const acceptedFields = ['content', 'noteUser', 'spotId', 'authorId', 'userName']
 
         const missingValues = acceptedFields.filter(field => !req.body[field])
         if (!isEmpty(missingValues)) {
@@ -16,13 +16,13 @@ api.post('/', async (req, res) => {
             })
         }
 
-        const { content, noteUser, postId, authorId, userName } = req.body
+        const { content, noteUser, spotId, authorId, userName } = req.body
 
         const post = await prisma.post.create({
             data: {
                 content,
                 note: noteUser,
-                postId: postId,
+                spotId: spotId,
                 authorId: authorId,
                 userName,
             }
