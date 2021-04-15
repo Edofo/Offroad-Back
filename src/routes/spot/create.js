b41327abaea70df4a14c4f1745bd7117dc045949
Edofo <Edofo@users.noreply.github.com>
2021-04-15 08:22:49 +0200

    Update create.js

--- src/routes/spot/create.js
+++ src/routes/spot/create.js
@@ -19,37 +19,65 @@ api.post('/', async (req, res) => {
 
         const { level, adress, infos, noteUser, content, authorId, userName } = req.body
 
-        axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+        axios.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json', {
             params:{
                 address: adress,
+                inputtype: textquery,
                 key: process.env.GOOGLE_API_KEY
             }
         })
-        .then(async function(response){
+        .then(async(response) => {
 
-            const spot = await prisma.spot.create({
-                data: {
-                    level,
-                    adress,
-                    infos,
-                    lat: response.data.results[0].geometry.location.lat,
-                    lng: response.data.results[0].geometry.location.lng,
-                    note: noteUser,
+            console.log('AH')
+            console.log(response)
+
+            axios.get('https://maps.googleapis.com/maps/api/place/photo', {
+                params: {
+                    photoreference: response.data.photos[0].photo_reference,
+                    maxwidth: 400,
+                    key: process.env.GOOGLE_API_KEY
                 }
             })
-            
-            const post = await prisma.post.create({
-                data: {
-                    content,
-                    note: noteUser,
-                    spotId: spot.id,
-                    authorId,
-                    userName
-                }
-            })
+            .then(async(response) => {
 
-            res.json({ data: { spot, post } })
+                console.log('BH')
+                console.log(response)
+
+                axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+                    params:{
+                        address: adress,
+                        key: process.env.GOOGLE_API_KEY
+                    }
+                })
+                .then(async(response) => {
+                    
+                    const spot = await prisma.spot.create({
+                        data: {
+                            level,
+                            adress,
+                            infos,
+                            lat: response.data.results[0].geometry.location.lat,
+                            lng: response.data.results[0].geometry.location.lng,
+                            note: noteUser,
+                        }
+                    })
+                    
+                    const post = await prisma.post.create({
+                        data: {
+                            content,
+                            note: noteUser,
+                            spotId: spot.id,
+                            authorId,
+                            userName
+                        }
+                    })
+
+                    res.json({ data: { spot, post } })
+                })
+            })
         })
+
+        
 
         
     } catch (err) {
