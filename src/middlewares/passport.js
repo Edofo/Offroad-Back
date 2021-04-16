006db42a0f7caffbdbaf788aa3c9112a30751857
Edofo <Edofo@users.noreply.github.com>
2021-04-16 13:35:49 +0200

    Update google

--- src/middlewares/passport.js
+++ src/middlewares/passport.js
@@ -105,7 +105,7 @@ passport.use(new GoogleStrategy({
           }
         })
 
-        next(null, profile);
+        next(null, profile, accessToken);
 
       } else {
 
--- src/routes/auth/google.js
+++ src/routes/auth/google.js
@@ -1,36 +1,17 @@
 import { Router } from 'express'
-import jwt from 'jsonwebtoken'
-import dotenv from 'dotenv'
 import passport from 'passport'
+import good from './good'
 
 const api = Router()
 
 api.get('/failed', (req, res) => res.send('You Failed to log in!'))
-api.get('/good', (req, res) => {
-  try {
-    const login = passport.authenticate('local', { session: false }, (err, user) => {
-      if (err) {
-        return res.status(400).json({ error: err })
-      }
-  
-      const { pseudo, email, id, level } = user
-      const payload = { pseudo, email, id, level }
-      dotenv.config()
-      const token = jwt.sign(payload, process.env.JWT_ENCRYPTION)
-      res.json({ data: { user, token } })
-    })
-  
-    login(req, res)
-  } catch (err) {
-    res.status(400).json({ error: err.message })
-  }
-})
+api.get('/good', (req, res) => res.send('Logged In!'))
 
 api.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }));
 api.get('/callback', passport.authenticate('google', { failureRedirect: 'failed' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('good')
+    res.json(req)
   }
 );
 
