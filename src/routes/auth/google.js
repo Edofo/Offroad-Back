5434aa99394fec858a26be34a1d34767f7d439aa
Edofo <Edofo@users.noreply.github.com>
2021-04-16 13:27:45 +0200

    Update google

--- src/routes/auth/google.js
+++ src/routes/auth/google.js
@@ -1,13 +1,30 @@
 import { Router } from 'express'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 import passport from 'passport'
-import good from './good'
 
 const api = Router()
 
 api.get('/failed', (req, res) => res.send('You Failed to log in!'))
-// api.get('/good', (req, res) => res.send('Logged In!'))
-
-api.use('/good', good)
+api.get('/good', (req, res) => {
+  try {
+    const login = passport.authenticate('local', { session: false }, (err, user) => {
+      if (err) {
+        return res.status(400).json({ error: err })
+      }
+  
+      const { pseudo, email, id, level } = user
+      const payload = { pseudo, email, id, level }
+      dotenv.config()
+      const token = jwt.sign(payload, process.env.JWT_ENCRYPTION)
+      res.json({ data: { user, token } })
+    })
+  
+    login(req, res)
+  } catch (err) {
+    res.status(400).json({ error: err.message })
+  }
+})
 
 api.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }));
 api.get('/callback', passport.authenticate('google', { failureRedirect: 'failed' }),
